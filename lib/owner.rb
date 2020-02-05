4e73c5612654a9e92007c21abb961beacba626c8
Abel Kalum <chumbabel@gmail.com>
2020-02-05 13:33:01 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,12 +61,16 @@ end
     self.cats.each {|cat| cat.mood = "happy"}
   end
 
- def sell_pets
-    self.pets.each do |pet|
-      self.pets.delete(pet)
-      pet.owner = nil
-      pet.mood = "nervous"
-    end
+ def sell_pets	
+        all_pets = self.cats + self.dogs	
+        all_pets.each do |pet|	
+            sell_pet(pet)	
+        end	
+    end	
+
+    def sell_pet(pet)	
+        pet.owner = nil	
+        pet.mood = "nervous"	
   end
   
   def list_pets
